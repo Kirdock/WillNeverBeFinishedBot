88559ad804d09e4e5965b96d6f9139778681a589
Klaus <k.striessnig@gmail.com>
2019-10-24 00:22:02 +0200

    certificate

--- server/webserver/server.js
+++ server/webserver/server.js
@@ -3,11 +3,14 @@ const express = require('express');
 const app = express();
 const body_parser = require('body-parser');
 const history = require('connect-history-api-fallback');
+const http = require('http');
+const https = require('https');
+const fs = require('fs');
 
 
 module.exports = (discordClient, config, logger)=> {
 
-    var cors = function (req, res, next) {
+    const cors = function (req, res, next) {
         res.header('Access-Control-Allow-Origin', '*');
         res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
         res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
@@ -21,17 +24,31 @@ module.exports = (discordClient, config, logger)=> {
         }
     };
     app.use(cors);
+    app.use(function(req, res, next) {
+        if((!req.secure) && (req.get('X-Forwarded-Proto') !== 'https') && req.get('Host').indexOf('localhost') < 0 && req.get('Host').indexOf('192.168.178.31') < 0) {
+            res.redirect('https://' + req.get('Host') + req.url);
+        }
+        else{
+            next();
+        }
+    });
     app.use(body_parser.json({limit: '20mb'}));
     const staticFileMiddleware = express.static(__dirname+'/../../dist');
     app.use(staticFileMiddleware);
     app.use(history());
     app.use(staticFileMiddleware);
 
-
-    var port = process.env.PORT || config.port;
-    var router = express.Router();
+    const router = express.Router();
 
     require('./routes.js')(router, logger, discordClient, config);
     app.use('/api',router);
-    app.listen(port);
+    const port = process.env.PORT || config.port;
+    
+    http.createServer(app).listen(port);
+    https.createServer({
+        key: fs.readFileSync(__dirname+'/cert/privkey.pem'),
+        cert: fs.readFileSync(__dirname+'/cert/cert.pem')
+    }, app)
+    .listen(port+1);
+    
 };
