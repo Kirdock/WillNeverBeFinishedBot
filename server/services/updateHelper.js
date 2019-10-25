e0ee2011b4c1c103cb9b3c38239ca81cfedb2ae4
Klaus <k.striessnig@gmail.com>
2019-10-26 00:49:41 +0200

    replaced shelljs

--- server/services/updateHelper.js
+++ server/services/updateHelper.js
@@ -1,7 +1,6 @@
 'use strict'
-const shell = require('shelljs')
+const spawn   = require('child_process').spawn;
 const q = require('q');
-
 
 module.exports = (config, logger) =>{
 
@@ -9,16 +8,12 @@ module.exports = (config, logger) =>{
         updateWebsite: updateWebsite
     };
 
-
     return updateHelper;
 
     function updateWebsite(){
         const defer = q.defer();
         try{
-            /*const child =*/ shell.exec(__dirname+'/../scripts/GitPull.sh',{async: true});
-            // child.stdout.on('data', function(data) {
-            //     defer.resolve(data);
-            // });
+            spawn(__dirname+'/../scripts/GitPull.sh');
             defer.resolve({success: true});
         }
         catch (error){
