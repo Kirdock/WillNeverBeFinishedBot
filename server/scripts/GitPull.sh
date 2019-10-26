cd90da27cafbc639065ebb08df5d28aab125f8d0
Klaus <k.striessnig@gmail.com>
2019-10-26 03:49:47 +0200

    Update GitPull.sh
    temporary correct PATH

--- server/scripts/GitPull.sh
+++ server/scripts/GitPull.sh
@@ -1,7 +1,12 @@
-#!/bin/bash
+#!/bin/bash 
+
 exec 3>&1 4>&2
 trap 'exec 2>&4 1>&3' 0 1 2 3
 exec 1>log.out 2>&1
+
+echo $PATH
+export PATH="$PATH:/opt/bin"
+echo $PATH
 
 git fetch
 UPSTREAM=${1:-'@{u}'}
